115245c9376c1fba2fc5131d0a99f89689b42826
Alexandre Zamberlan <alexz@ufn.edu.br>
2023-05-19 11:09:42 -0300

    settings.py com allowed_hosts

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=%6aqk0p^aux0qvolqn_7efyj(@wh*wtc_!n10u8_o4!l#k6)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'longevos.lapinf.ufn.edu.br', 'www.longevos.lapinf.ufn.edu.br']
 
 AUTH_USER_MODEL = 'usuario.Usuario'
 
